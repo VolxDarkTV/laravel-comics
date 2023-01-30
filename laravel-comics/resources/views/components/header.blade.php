6de90f5869b762b7e7a7118ecf7e634a75caba1a
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-01-30 16:34:49 +0100

    first commit

---
+++ laravel-comics/resources/views/components/header.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+
+</head>
+<body>
+    <section class="main_container">
+        <div class="container">
+            <div>
+                <img src="{{Vite::asset('/resources/img/dc-logo.png')}}" alt="">
+            </div>
+    
+            <!-- Links -->
+            <ul>
+                <li>
+                    <a>CHARACTERS</a>
+                    <div class="under_line"></div>
+                </li>
+                <li>
+                    <a>COMICS</a>
+                    <div class="under_line"></div>
+                </li>
+                <li>
+                    <a>MOVIES</a>
+                    <div class="under_line"></div>
+                </li>
+                <li>
+                    <a>TV</a>
+                    <div class="under_line"></div>
+                </li>
+                <li>
+                    <a>GAMES</a>
+                    <div class="under_line"></div>
+                </li>
+                <li>
+                    <a>COMICS</a>
+                    <div class="under_line"></div>
+                </li>
+                <li>
+                    <a>COMICS</a>
+                    <div class="under_line"></div>
+                </li>
+                <li>
+                    <a>COMICS</a>
+                    <div class="under_line"></div>
+                </li>
+                <li>
+                    <a>COMICS</a>
+                    <div class="under_line"></div>
+                </li>
+                <li>
+                    <a>COMICS</a>
+                    <div class="under_line"></div>
+                </li>
+            </ul>
+        </div>
+    </section>
+</body>
+</html>
---
+++ laravel-comics/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts/main')
+
+@section('head')
+    <title>Home</title>
+@endsection
+
+
+@section('main')
+    <h1>hello</h1>
+@endsection
